fae74d7112d2cf5523a013e5e2d104e8d25658ca
Jim Salyer <jim.salyer@gmail.com>
2024-08-23 14:54:22 -0500

    Tweak shared environment variables. JS

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
-export GH_HOST="https://github.com"
-export NPM_HOST="https://registry.npmjs.org"
-export PIP_HOST="https://pypi.org/simple"
+export GH_HOST=git@github.com
+export GH_PUBLIC_HOST=https://github.com
+export NPM_HOST=https://registry.npmjs.org
+export PIP_HOST=https://pypi.org/simple
