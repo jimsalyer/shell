621e8aa53362e4d10c36fa95f4407ed894401970
Jim Salyer <jim.salyer@efleets.com>
2025-02-28 10:22:48 -0600

    Get run_prettier function to actually work. JS

--- scripts/run-prettier.sh
+++ scripts/run-prettier.sh
@@ -9,14 +9,34 @@
 
 run_prettier() {
   local file="$1"
-  if [[ -d "$(dirname "$file")/node_modules/prettier" ]]; then
-    echo "Running local version of Prettier"
-    node "$(dirname "$file")/node_modules/prettier/bin-prettier.js" -w "$file"
-  elif [[ "$(command -v prettier)" ]]; then
-    echo "Running global version of Prettier"
-    prettier -w "$file"
+  if [[ -f "$file" ]]; then
+    local curr_dir
+    local package_dir
+
+    curr_dir="$(dirname "$(realpath "$file")")"
+    while [[ -d "$curr_dir" && -z "$package_dir" ]]; do
+      [[ -f "$curr_dir/package.json" ]] && package_dir="$curr_dir"
+      curr_dir="${curr_dir%/*}"
+    done
+
+    if [[ -d "$package_dir" ]]; then
+      local prettier_dir
+      prettier_dir="$package_dir/node_modules/prettier"
+
+      if [[ -d "$prettier_dir" ]]; then
+        echo "Running local version of Prettier"
+        node "$prettier_dir/bin-prettier.js" -w "$file"
+      elif [[ "$(command -v prettier)" ]]; then
+        echo "Running global version of Prettier"
+        prettier -w "$file"
+      else
+        echo "Running remote version of Prettier"
+        npx prettier -w "$file"
+      fi
+    else
+      echo "Could not find any package.json files at or above \"$file\"."
+    fi
   else
-    echo "Running remote version of Prettier (npx)"
-    npx prettier -w "$file"
+    echo "The file \"$file\" does not exist."
   fi
 }
