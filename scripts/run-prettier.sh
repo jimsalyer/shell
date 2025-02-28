c686bd6e42d94273205bfe91d0d4b6d1c8577158
Jim Salyer <jim.salyer@efleets.com>
2025-02-28 10:55:24 -0600

    Use prettier shell script instead of source file. JS

--- scripts/run-prettier.sh
+++ scripts/run-prettier.sh
@@ -11,30 +11,23 @@ run_prettier() {
   local file="$1"
   if [[ -f "$file" ]]; then
     local curr_dir
-    local package_dir
+    local prettier
 
     curr_dir="$(dirname "$(realpath "$file")")"
-    while [[ -d "$curr_dir" && -z "$package_dir" ]]; do
-      [[ -f "$curr_dir/package.json" ]] && package_dir="$curr_dir"
+    while [[ -d "$curr_dir" && -z "$prettier" ]]; do
+      [[ -f "$curr_dir/node_modules/.bin/prettier" ]] && prettier="$curr_dir/node_modules/.bin/prettier"
       curr_dir="${curr_dir%/*}"
     done
 
-    if [[ -d "$package_dir" ]]; then
-      local prettier_dir
-      prettier_dir="$package_dir/node_modules/prettier"
-
-      if [[ -d "$prettier_dir" ]]; then
-        echo "Running local version of Prettier"
-        node "$prettier_dir/bin-prettier.js" -w "$file"
-      elif [[ "$(command -v prettier)" ]]; then
-        echo "Running global version of Prettier"
-        prettier -w "$file"
-      else
-        echo "Running remote version of Prettier"
-        npx prettier -w "$file"
-      fi
+    if [[ -f "$prettier" ]]; then
+      echo "Running local version of Prettier"
+      "$prettier" -w "$file"
+    elif [[ "$(command -v prettier)" ]]; then
+      echo "Running global version of Prettier"
+      prettier -w "$file"
     else
-      echo "Could not find any package.json files at or above \"$file\"."
+      echo "Running remote version of Prettier"
+      npx prettier -w "$file"
     fi
   else
     echo "The file \"$file\" does not exist."
