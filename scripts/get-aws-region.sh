89844b78afd839bd0d527b8cb109ca44de4ebd16
Jim Salyer <jim.salyer@efleets.com>
2025-04-01 17:22:38 -0500

    Add more flexibility to aws-get-region script. JS

--- scripts/get-aws-region.sh
+++ scripts/get-aws-region.sh
@@ -8,11 +8,38 @@
 # =============================================================================
 
 get_aws_region() {
-  if [[ -z "$AWS_REGION" ]]; then
-    echo "AWS region not set. Retrieving..."
-    AWS_REGION=$(aws configure get region)
+  if [[ -n "$AWS_REGION" ]]; then
+    echo "$AWS_REGION"
+    return 0
+  elif [[ -n "$AWS_DEFAULT_REGION" ]]; then
+    echo "$AWS_DEFAULT_REGION"
+    return 0
+  elif [[ -n "$AWS_CONFIG_REGION" ]]; then
+    echo "$AWS_CONFIG_REGION"
+    return 0
+  elif [[ -n "$AWS_EC2_REGION" ]]; then
+    echo "$AWS_EC2_REGION"
+    return 0
   fi
 
-  export AWS_REGION
-  echo "$AWS_REGION"
+  echo "AWS region not set. Getting from current configuration..." >&2
+  AWS_CONFIG_REGION="$(aws configure get region)"
+
+  if [[ -n "$aws_config_region" ]]; then
+    export AWS_CONFIG_REGION
+    echo "$AWS_CONFIG_REGION"
+    return 0
+  fi
+
+  echo "AWS region not configured. Getting from EC2..." >&2
+  AWS_EC2_REGION="$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].RegionName')"
+
+  if [[ -n "$AWS_EC2_REGION" ]]; then
+    export AWS_EC2_REGION
+    echo "$AWS_EC2_REGION"
+    return 0
+  fi
+
+  echo "AWS region not available." >&2
+  return 1
 }
