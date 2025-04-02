f574f0850003699891165b912ada4924b0e3afa5
Jim Salyer <jim.salyer@efleets.com>
2025-04-02 11:35:46 -0500

    Delete unneeded templates. JS

--- templates/.bashrc
+++
@@ -1,7 +0,0 @@
-. "$HOME/shell/env.sh"
-. "$HOME/shell/functions.sh"
-. "$HOME/shell/aliases.sh"
-
-ensure_interactive
-load_dircolors "$HOME/dracula/dircolors/.dircolors"
-load_posh bash "$HOME/shell/config/omp.json"
--- templates/init.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-doskey disable_init=reg delete "HKCU\Software\Microsoft\Command Processor" /v AutoRun
-doskey enable_init=reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d "%"USERPROFILE"%\init.cmd" /f
-doskey refresh_env=%USERPROFILE%\shell\refresh_env.cmd
-
-endlocal
