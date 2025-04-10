e4fa6af35ac123ac9b922f41482e19d33d90dd61
Jim Salyer <jim.salyer@gmail.com>
2025-04-10 17:01:03 -0500

    Use more reliable shell timer. JS

--- functions/timer.sh
+++ functions/timer.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2005
 
 # Timer functions to get elapsed time on a given piece of logic
 
-_date() {
-  if [[ "$OSTYPE" == darwin* ]]; then
-    echo "$(gdate "$1")"
-  else
-    echo "$(date "$1")"
-  fi
+function start_timer() {
+  timer=$(($(print -P "%D{%s%6.}") / 1000))
 }
 
-start_timer() {
-  timer_name="'$1' timer"
-  timer_name="${timer_name/\'\' t/T}"
-  timer="$(($(_date +%s%0N) / 1000000))"
-  echo "$timer_name started"
-}
+function stop_timer() {
+  if [[ $timer ]]; then
+    local now=$(($(print -P "%D{%s%6.}") / 1000))
+    local d_ms=$(($now - $timer))
+    local d_s=$((d_ms / 1000))
+    local ms=$((d_ms % 1000))
+    local s=$((d_s % 60))
+    local m=$(((d_s / 60) % 60))
+    local h=$((d_s / 3600))
 
-stop_timer() {
-  if [[ "$timer" ]]; then
-    now="$(($(_date +%s%0N) / 1000000))"
-    elapsed="$((now - timer))"
+    if ((h > 0)); then
+      elapsed=${h}h${m}m
+    elif ((m > 0)); then
+      elapsed=${m}m${s}s
+    elif ((s > 9)); then
+      elapsed=${s}.$(printf %03d $ms | cut -c1-2)s # 12.34s
+    elif ((s > 0)); then
+      elapsed=${s}.$(printf %03d $ms)s # 1.234s
+    else
+      elapsed=${ms}ms
+    fi
 
-    echo "$timer_name ran for ${elapsed}ms"
+    echo "$elapsed"
     unset timer
-    unset timer_name
   fi
 }
